21ee846b70484bc0799f3ba6e7d0a6c9ee675c95
Guilherme Silveira <guilhermesilveirasousa@gmail.com>
2025-05-23 16:46:49 -0300

    feat: add default admin

---
+++ question-bank-backend/src/QuestionBank.Infra/Migrations/20250523192936_AddDefaultAdmin.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuestionBank.Infra.Migrations
+{
+    public partial class AddDefaultAdmin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var password = "$argon2id$v=19$m=32768,t=4,p=1$8kSN61J8u9f2fBanH2sbjA$mcjis6H1GOwjNVVNBznVkOkktsa+CHUc9bP95x8IsEo";
+            
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Name", "Email", "Password", "IsAdmin" },
+                values: new object[,]
+                {
+                    { 1, "Administrator", "admin@admin.com", password, true }
+                }
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1
+            );
+        }
+    }
+}
